ee57ada8aec3bc531a69e8ba0e0a9cca9dd79f38
HanzBOTZ <100819969+HANZBOTZGZ@users.noreply.github.com>
2022-03-19 14:26:04 +0700

    Update script.js

--- plugins/script.js
+++ plugins/script.js
@@ -1,7 +1,7 @@
 let handler  = async (m, { conn, usedPrefix }) => {
   conn.reply(m.chat, `
 *HanzBOTZ Menggunakan Script*
-github.com/HANZBOTZGZ/lordbot-aq-tes
+github.com/AmmarBN/lordbot-aq-tes
 _*Jangan Lupa Follow Sama Kasi Star Ya kak*_
 
 `.trim(), m)
